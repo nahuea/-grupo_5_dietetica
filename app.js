47aee0b366ddfca6ab06e9391fd1a2b9e4f9b2d5
Nicolas Valente <valente_nicolas@hotmail.com>
2021-07-31 16:01:50 -0300

    pagina de registro terminada

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000;
 const path = require('path')
 app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, 'views/index.html'))
@@ -25,6 +25,6 @@ app.use(express.static('views'))
 app.get('*', function(request, response) {
     response.send('NOT FOUND', 404)
 })
-app.listen(port, function() {
-    console.log('Servido corriendo en corriendo en el puerto ' + port  );
+app.listen(port, ()=> {
+    console.log('Servido corriendo en http://localhost:' + port);
 })
