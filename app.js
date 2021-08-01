202f10cba6fc1bc6093c7787609136ef7d6b113e
nahuea <nahuelandrada94@gmail.com>
2021-08-01 04:55:43 -0300

    seccion

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ app.get('/register', function(request,response){
 app.get('/login', function(request,response){
     response.sendFile(path.join(__dirname,'views/login.html'))
 })
-app.get('/producto', function(request,response){
-    response.sendFile(path.join(__dirname,'views/producto.html'))
+app.get('/productDetail', function(request,response){
+    response.sendFile(path.join(__dirname,'views/productDetail.html'))
 })
-app.get('/carrito', function(request,response){
-    response.sendFile(path.join(__dirname,'views/carrito.html'))
+app.get('/productCart', function(request,response){
+    response.sendFile(path.join(__dirname,'views/productCart.html'))
 })
 app.get('/seccion', function(request,response){
     response.sendFile(path.join(__dirname,'views/seccion.html'))
@@ -26,5 +26,5 @@ app.get('*', function(request, response) {
     response.send('NOT FOUND', 404)
 })
 app.listen(port, ()=> {
-    console.log('Servido corriendo en http://localhost:' + port);
+    console.log('Servidor corriendo en http://localhost:' + port);
 })
