21a2dd9b1b002a14c3fc3969c2b2b907868a2445
Nicolas Valente <valente_nicolas@hotmail.com>
2021-08-11 15:52:08 -0300

    ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000;
 const path = require('path')
+app.set('view engine' , 'ejs');
 app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, 'views/index.html'))
 })
