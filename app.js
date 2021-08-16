78984e4e98d73aca92a6d80866685033f00e9d01
frangomar <frangomar8@gmail.com>
2021-08-16 18:05:03 -0300

    corregi el head.ejs y empece a modificar el index

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const app = express()
 const port = 3000;
 const path = require('path')
 app.set('view engine' , 'ejs');
-app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname, 'views/index.html'))
+app.get('/', function(req, res) {
+    res.render(path.join(__dirname, 'views/index.ejs'))
 })
-app.get('/register', function(request,response){
-    response.sendFile(path.join(__dirname,'views/register.html'))
+app.get('/register', function(req,res){
+    res.sendFile(path.join(__dirname,'views/register.html'))
 })
-app.get('/login', function(request,response){
-    response.sendFile(path.join(__dirname,'views/login.html'))
+app.get('/login', function(req,res){
+    res.sendFile(path.join(__dirname,'views/login.html'))
 })
-app.get('/productDetail', function(request,response){
-    response.sendFile(path.join(__dirname,'views/productDetail.html'))
+app.get('/productDetail', function(req, res){
+    res.sendFile(path.join(__dirname,'views/productDetail.html'))
 })
-app.get('/productCart', function(request,response){
-    response.sendFile(path.join(__dirname,'views/productCart.html'))
+app.get('/productCart', function(req,res){
+    rese.sendFile(path.join(__dirname,'views/productCart.html'))
 })
-app.get('/seccion', function(request,response){
-    response.sendFile(path.join(__dirname,'views/seccion.html'))
+app.get('/seccion', function(req,res){
+    res.sendFile(path.join(__dirname,'views/seccion.html'))
 })
 app.use(express.static('public'))
 app.use(express.static('views'))
