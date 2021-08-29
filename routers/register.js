afb978d82ac07c681a305464b93f326be7a23311
Nicolas Valente <valente_nicolas@hotmail.com>
2021-08-29 17:31:09 -0300

    multer del registro terminado

--- routers/register.js
+++ routers/register.js
@@ -2,13 +2,27 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer');
 const controller = require('../controllers/registerController');
-const storage = multer.diskStorage
+const path = require('path');
+const { dirname } = require('path');
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null,path.join(__dirname, '../public/img/register'))
+    },
+    filename :(req,file,cb) =>{
+        const newFilename = "";
+        cb(null,newFilename );
+
+    
+    } 
+});
+
+const upload = multer({ storage:storage });
 
 
 router.get('/',controller.register) 
 
 router.get('/create' , controller.create)
-router.post('/' , controller.store)
+router.post('/' , upload.single('imagen-de-usuario'), controller.store)
 
 
 module.exports = router
